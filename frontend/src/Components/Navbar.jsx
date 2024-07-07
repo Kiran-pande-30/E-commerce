aba9761868aec00e5905168b94ec9caf486343f4
Kiran-pande-30 <kpande026@gmail.com>
2024-07-07 20:05:21 +0530

    rough responsiveness

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 // Navbar.jsx
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Navbar.css';
 
@@ -7,17 +7,25 @@ import '../style/Navbar.css';
 import logo from '../Assets/Admin_Assets/nav-logo.svg';
 import cart_icon from '../Assets/Admin_Assets/Product_Cart.svg';
 import { ShopContext } from '../Context/ShopContext';
+import nav_dropdown from '../Assets/Frontend_Assets/nav_dropdown.png'
 
 export const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdowm_toggle = (e) => {
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt='' size={34} />
       </div>
-      <ul className="nav-menu">
+      <img className='nav-dropdown' onClick={dropdowm_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={() => setMenu("shop")}>
           <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
           {menu === "shop" ? <hr /> : null}
