d6495faaaa8afe982f21eb70510a6305eedaf271
Kiran-pande-30 <kpande026@gmail.com>
2024-07-09 02:15:27 +0530

    admin setup

---
+++ admin/src/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './Admin.css'
+
+const admin = () => {
+  return (
+    <div className='admin'>
+
+    </div>
+  )
+}
+
+export default admin
